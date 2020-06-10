53d0fa480fcaceb56e7ca4b080d9926f52145e7f
Angel Pizarro <delagoya@gmail.com>
2020-06-10 08:25:34 -0400

    DDB table create fixes, add config env

--- createtable.py
+++ createtable.py
@@ -3,21 +3,10 @@ import boto3
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
-
-table_exists = False
-#IF table exists, then reset the entries
-for table in dynamodb.tables.all():
-    if table.name == "md5counter":
-        table_exists = True
-        print("Table already exists, resetting the counters.")
-        table.delete_item(Key={ "status": "success"})
-        table.delete_item(Key={ "status": "error"})
-        table.put_item(Item={ "status": "success","n": 0})
-        table.put_item(Item={ "status": "error","n": 0})
-        print("Counters reset.")
-        break
-# IF the table does not exist, then create and populate
-if not table_exists:
+table = dynamodb.Table("md5counter")
+try:
+    table.creation_date_time
+except:
     print("Table does not exist, creating ...")
     # Create the DynamoDB table.
     table = dynamodb.create_table(
@@ -34,14 +23,11 @@ if not table_exists:
                 'AttributeType': 'S'
             }
         ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
+        BillingMode='PAY_PER_REQUEST'
     )
 
     # Wait until the table exists.
-    table.meta.client.get_waiter('table_exists').wait(TableName='md5counter')
+    table.wait_until_exists()
 
     # Print out some data about the table.
     print("Table 'md5counter' created.")
